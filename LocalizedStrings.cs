b00ee5b6eb5e7c0ffb10f382612dfd489ab0c14d
rommel234 <rommelkms@outlook.com>
2016-03-30 16:28:46 +0300

    Added files via upload
    Mobile app of handy man 2.o. main , login an registration pages.
    with basic login access to login and registration page.

---
+++ LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using handy_man_2._0.Resources;
+
+namespace handy_man_2._0
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
